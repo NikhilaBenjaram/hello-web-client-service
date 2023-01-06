c2ac04256a192a507c8d0db256dfdeacf947ea87
ikrahim <100006054+IkramuddinMohammad@users.noreply.github.com>
2023-01-06 16:22:29 -0500

    Initial commit

---
+++ src/test/java/com/example/hellowebclientservice/HelloWebClientServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.hellowebclientservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloWebClientServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
